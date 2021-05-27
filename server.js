9acaa0dcde3bc9662e5c7feb8952646347f356bc
Joyia <joyia418@gmail.com>
2021-05-27 12:45:59 +0500

    3rd

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const mongoose = require('mongoose');
 const router = require('./routes/index');
 
 const app = express(); 
-const MONGODB_URI = "mongodb://localhost:27017/my_local_db"; 
-
 // app.use(cors())
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use('/api', router); 
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false }); 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false }); 
 mongoose.connection.once('open', function() { 
   console.log('Connected to the Database.');
 });
@@ -19,6 +17,6 @@ mongoose.connection.on('error', function(error) {
   console.log('Mongoose Connection Error : ' + error);
 });
 
-app.listen(PORT, function() { 
+app.listen(process.env.PORT, function() { 
   console.log(`Server listening on port ${PORT}.`);
 });
